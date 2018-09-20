604f6c9c3f8bf30c70c0097ba2a7063d6f6fc42c
John-XtremeApp <johnlin@xtreme.com.tw>
2018-09-20 16:06:38 +0800

    [Link]整理分享連結

--- vue/src/components/Links.vue
+++ vue/src/components/Links.vue
@@ -1,15 +1,10 @@
 <template lang="html">
 <div>
+    <!-- 關於本站，width < 768px 時才會顯示 -->
     <a 
         title="關於本站" 
-        href="#"
-        class="visible-xs pull-left ebas btn btn-default"
-        style="
-            float: left;
-            margin-top: -10px;
-            margin-left: 5px;
-            margin-bottom: 5px;
-        "   
+        href="/about.html"
+        class="visible-xs pull-left ebas btn btn-default about"
     >
     <!-- onClick={ ()=>window.pressAbout() }  -->
         <span class="iconic-circle">
@@ -19,22 +14,16 @@
             &nbsp;萌典
         </span>
     </a>
+    <!-- 關於本站，width < 768px 時才會顯示 -->
 
-    <div 
-        class="share" 
-        style="
-            float: right;
-            margin-top: -10px;
-            margin-right: 5px;
-            margin-bottom: 15px;
-        "
-    >
+    <!-- Fb, Twitter, Google Button -->
+    <div class="share-btn-list">
         <a 
             v-for="(item) in shareButtons"
             :key="item.id"
             :id="'share-' + item.id"
             :title="item.label + '分享' "
-            :data-href="item.href"
+            :href="item.href"
             :style="{
                 background: item.background,
                 color: 'white'
@@ -46,6 +35,7 @@
             <i :class="'icon-'+item.icon" />
         </a>
     </div>
+    <!-- Fb, Twitter, Google Button -->
 </div>
 </template>
 
@@ -81,6 +71,27 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style lang="scss" scoped>
+
+// 關於本站 
+.about {
+    float: left;
+    margin-top: -10px;
+    margin-left: 5px;
+    margin-bottom: 5px;
+
+    // 關於本站按鈕的 icon
+    .iconic-circle {
+        background: #474;
+    }
+}
+
+// 分享按鈕
+.share-btn-list {
+    float: right;
+    margin-bottom: 15px;
+    margin-right: 5px;
+    margin-top: -10px;
+}
 
 </style>
