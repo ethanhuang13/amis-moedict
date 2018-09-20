5fa51f8fbc03621f2f3f6021410b6f113bcf0e7a
John-XtremeApp <johnlin@xtreme.com.tw>
2018-09-20 16:04:59 +0800

    [Webpack]升級 webpack babel 以及許多相關套件 僅調整設定適用於 vue， 原本的代碼在新的 version 下跑不動

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,34 @@
-var path = require('path');
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const src = path.resolve(__dirname, 'src');
 var webpack = require('webpack');
 var entries = [];
 var loaders = [];
 var plugins = [];
 if (/production/.test(process.env.NODE_ENV)) {
-    plugins = [ new webpack.optimize.UglifyJsPlugin() ];
+    plugins = [ 
+        // new VueLoaderPlugin(),
+        new webpack.optimize.UglifyJsPlugin()
+    ];
 }
 else {
-    entries = [ 'webpack-dev-server/client?http://localhost:8888', 'webpack/hot/dev-server' ];
-    loaders = [ { loader: 'react-hot', exclude: /node_modules/ } ]
-    plugins = [ new webpack.HotModuleReplacementPlugin() ];
+    entries = [ 'webpack-dev-server/client?http://localhost:8888' ];
+    loaders = [ 
+        // { 
+        //     loader: 'react-hot-loader', 
+        //     exclude: /node_modules/ 
+        // } 
+    ]
+    plugins = [ 
+        new VueLoaderPlugin(),
+        // new webpack.HotModuleReplacementPlugin() 
+    ];
 }
 
 module.exports = {
+    mode: 'development',
     entry: entries.concat([
-        './main.js',
+        './vue/src/main.js',
         './js/jquery-ui-1.10.4.custom.min.js',
         './js/jquery.hoverIntent.js',
         './js/bootstrap/dropdown.js',
@@ -28,9 +42,43 @@ module.exports = {
     },
     plugins: plugins,
     module: {
-        loaders: loaders.concat([
-            { test: /\.jsx?$/, loader: 'babel?stage=0', exclude: /node_modules/ },
-            { test: /\.json$/, loader: 'json' }
+        rules: loaders.concat([
+            // { 
+            //     // babel-loader, will use the settings in .babelrc
+            //     test: /\.jsx?$/, 
+            //     loader: 'babel-loader', 
+            //     exclude: /node_modules/
+            // },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            { 
+                // babel-loader, will use the settings in .babelrc
+                test: /\.js?$/, 
+                loader: 'babel-loader', 
+                exclude: /node_modules/
+            },
+            {        
+                test: /\.css$/,        
+                use:[          
+                    'vue-style-loader',          
+                    'css-loader',        
+                ]     
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "vue-style-loader", 
+                    "css-loader", 
+                    "sass-loader"
+                ]
+            },
+
+            { 
+                test: /\.json$/, 
+                loader: 'json-loader' 
+            }
         ])
     },
 }
